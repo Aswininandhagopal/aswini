1c8ffdecc66ecaa502271f0351493de3fa8cea48
Aswininandhagopal <aswininandhagopal@gmail.com>
2016-10-21 12:10:49 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int power_of_2(int n);
+int main()
+{
+unsigned int number;
+printf("\n enter number");
+scanf("%d",&num);
+power_of_2(int number)
+}
+int power_of_2(int n)
+{
+int i,reminder,a=0,b=0;
+for(i=0;i<number/2;i++)
+number=n/2;
+if(reminder==0)
+a++;
+else
+b++;
+}
+if((a%2==0)&&(b==0))
+printf("\nThe number is a power of two");
+else
+printf("\nThe number is not a power of two");
+}
